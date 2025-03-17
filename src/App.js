7a1c51f2e859259d05b10ec70062affbb1fb08a9
Igor Lima Ponce <Igorlima0748@gmail.com>
2025-03-17 20:46:23 -0300

    Primeiro-Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import './style.css';
 
 function App() {
+  const [pokemon, setPokemon] = useState(null);
+
+  function loadAPI() {
+    let url = 'https://pokeapi.co/api/v2/pokemon/zorua';
+    fetch(url)
+      .then(response => response.json())
+      .then(json => setPokemon(json))
+      .catch(err => console.log(err));
+  }
+
+  useEffect(() => {
+    loadAPI();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <strong> Pokedex </strong>
       </header>
+
+      {pokemon ? (
+        <div>
+          <img src={pokemon.sprites.front_default} alt={pokemon.name} />
+          <div> Name: {pokemon.name}</div>
+          <div> N: {pokemon.id}</div>
+          <div> Peso: {pokemon.weight / 10}kg</div>
+          <div> Altura: {pokemon.height / 10}m</div>
+        </div>
+      ) : (
+        <p>Carregando...</p>
+      )}
     </div>
   );
 }
